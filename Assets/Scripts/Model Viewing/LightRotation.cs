3db17703f041689ca09b9da7ebbf60ad9b27b0e2
Sebastian S.A. Cooper <ssac95@gmail.com>
2019-08-24 21:59:48 -0500

    Created AR, FileView and Editor Scenes
    (As well as a rotation script so the light source will always be pointing down)

---
+++ Assets/Scripts/Model Viewing/LightRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightRotation : MonoBehaviour
+{
+
+    public void LateUpdate()
+    {
+        if(!Input.isGyroAvailable)
+            return;
+
+        transform.rotation = Down();
+    }
+
+    private void Down()
+    {
+        Quaternion phoneRotation = Input.gryo.attitude;
+        Quaternion differenceFromDown = Vector3.down - phoneRotation;
+        return differenceFromDown;
+    }
+}
